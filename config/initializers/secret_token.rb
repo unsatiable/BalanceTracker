55a1bb0781115ba2079297ded4f3ffc042d2f68b
unsatiable <amadsen87@gmail.com>
2015-02-17 18:25:28 -0700

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalanceTracker::Application.config.secret_token = 'df2ad6e5d21e3fe2081ec47bd5436b8edc57d59c88e1db444605bfeb25d5ee0d72f9e68da0e2ad3926d863e0af728e9a18c7b0b5608d82dcdd0513f137b7b255'
